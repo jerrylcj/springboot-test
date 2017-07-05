a8af5a488b1ee6faa1781329a2ddcbe18cac110b
jerrylcj <jerry_lcj@163.com>
2017-07-05 14:18:28 +0800

    edit

--- springboot-test/src/main/java/com/jerry/test/springboottest/SampleController.java
+++ springboot-test/src/main/java/com/jerry/test/springboottest/SampleController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @EnableAutoConfiguration
 public class SampleController {
 
-    @RequestMapping("/")
+    @RequestMapping("/home")
     @ResponseBody
     String home() {
         return "Hello World!";
--- springboot-test/src/main/java/com/jerry/test/web/HelloWorldController.java
+++ springboot-test/src/main/java/com/jerry/test/web/HelloWorldController.java
@@ -1,13 +1,23 @@
 package com.jerry.test.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableAutoConfiguration
 public class HelloWorldController {
 
 	@RequestMapping("/hello")
-	public String index(){
+	@ResponseBody
+	public String home(){
 		return "Hello World";
 	}
+	
+	
+	public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloWorldController.class, args);
+    }
 }
